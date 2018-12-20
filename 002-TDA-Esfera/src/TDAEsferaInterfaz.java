4870c29b4498a4a64afc5415dbcf839487cc1142
Akin Ramirez <akin.ramirez@hotmail.com>
2018-12-20 10:15:48 -0600

    002-TDA-Esfera

---
+++ 002-TDA-Esfera/src/TDAEsferaInterfaz.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author Akin Ramirez
+ */
+public class TDAEsferaInterfaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TDAEsferaImplementacion esferita = new TDAEsferaImplementacion(4);
+        System.out.println("Radio: " + esferita.getRadio());
+        System.out.println("Diametro: " + esferita.getDiametro());
+        System.out.println("Circuferencia: " + esferita.getCircuferencia());
+        System.out.println("Area: " + esferita.getArea());
+        System.out.println("Volument: " + esferita.getVolumen());
+    }
+    
+}
